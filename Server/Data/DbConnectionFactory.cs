e42f554e384e9db2d0c998e106f8a9cdff01b7d5
John Doe <johndoe@example.com>
2025-06-16 20:53:57 +0300

    Добавлена фабрика для создания соединений с БД

---
+++ Server/Data/DbConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Server.Data;
+
+/// <summary>
+/// Фабрика для создания подключений к базе данных
+/// </summary>
+public class DbConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public DbConnectionFactory()
+    {
+        _connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=AvgElbrusEnjoyerDb;Trusted_Connection=True;";
+    }
+
+    /// <summary>
+    /// Создает и возвращает новое подключение к БД
+    /// </summary>
+    /// <returns>Объект подключения, реализующий IDbConnection</returns>
+    public IDbConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
