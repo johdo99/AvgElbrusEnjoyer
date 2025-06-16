b3119b2bac492f5680673a857822615254acade6
John Doe <johndoe@example.com>
2025-06-16 21:19:04 +0300

    Добавлен API для получения списка компонентов

---
+++ Server/Api/CatalogController.cs
@@ -0,0 +1,41 @@
+﻿using Server.Services;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Server.Api;
+
+public class CatalogController
+{
+    private readonly ICatalogService _catalogService;
+
+    public CatalogController(ICatalogService catalogService)
+    {
+        _catalogService = catalogService;
+    }
+
+    public async Task HandleRequestAsync(HttpListenerContext context)
+    {
+        var request = context.Request;
+        var response = context.Response;
+
+        if (request.HttpMethod != "GET")
+        {
+            response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+            response.OutputStream.Close();
+            return;
+        }
+
+        var components = await _catalogService.GetAllComponentsAsync();
+
+        response.StatusCode = (int)HttpStatusCode.OK;
+        response.ContentType = "application/json";
+
+        var jsonResponse = JsonSerializer.Serialize(components);
+        var buffer = System.Text.Encoding.UTF8.GetBytes(jsonResponse);
+
+        response.ContentLength64 = buffer.Length;
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+        response.OutputStream.Close();
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -11,9 +11,14 @@ public class Program
     public static async Task Main(string[] args)
     {
         var dbConnectionFactory = new DbConnectionFactory();
+
         IUserRepository userRepository = new UserRepository(dbConnectionFactory);
         IAuthService authService = new AuthService(userRepository);
         var authController = new AuthController(authService);
+
+        IComponentRepository componentRepository = new ComponentRepository(dbConnectionFactory);
+        ICatalogService catalogService = new CatalogService(componentRepository);
+        var catalogController = new CatalogController(catalogService);
 
         var listener = new HttpListener();
         listener.Prefixes.Add("http://localhost:8888/");
@@ -29,6 +34,10 @@ public class Program
             if (request.Url.AbsolutePath.StartsWith("/api/auth"))
             {
                 await authController.HandleRequestAsync(context);
+            }
+            else if (request.Url.AbsolutePath.StartsWith("/api/catalog"))
+            {
+                await catalogController.HandleRequestAsync(context);
             }
             else
             {
