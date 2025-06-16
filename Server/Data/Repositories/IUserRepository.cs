ddeabf61c764a5d63a3154f8302103217953ea4c
John Doe <johndoe@example.com>
2025-06-16 21:01:26 +0300

    Добавлен репозиторий для работы с пользователями

---
+++ Server/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Server.Models;
+using System.Threading.Tasks;
+
+namespace Server.Data.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByUsernameAsync(string username);
+    Task<int> AddAsync(User user);
+}
--- Server/Data/Repositories/UserRepository.cs
+++ Server/Data/Repositories/UserRepository.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using Server.Models;
 
-namespace Server.Data.Repositories
+namespace Server.Data.Repositories;
+
+public class UserRepository : IUserRepository
 {
-    internal class UserRepository
+    private readonly DbConnectionFactory _dbConnectionFactory;
+
+    public UserRepository(DbConnectionFactory dbConnectionFactory)
     {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task<int> AddAsync(User user)
+    {
+        using var connection = _dbConnectionFactory.CreateConnection();
+
+        var sql = """
+                  INSERT INTO Users (Username, PasswordHash, Role)
+                  VALUES (@Username, @PasswordHash, @Role);
+                  SELECT CAST(SCOPE_IDENTITY() as int);
+                  """;
+
+        // Dapper выполняет запрос и передает параметры из объекта user
+        var newId = await connection.QuerySingleAsync<int>(sql, user);
+        return newId;
+    }
+
+    public async Task<User?> GetByUsernameAsync(string username)
+    {
+        using var connection = _dbConnectionFactory.CreateConnection();
+
+        var sql = "SELECT * FROM Users WHERE Username = @Username";
+
+        // Dapper выполняет запрос и автоматически создает объект User из результата
+        return await connection.QuerySingleOrDefaultAsync<User>(sql, new { Username = username });
     }
 }
